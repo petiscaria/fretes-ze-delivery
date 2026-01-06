28e40742299c4e251ddeea8213ca41e63a2122db
petiscaria <gilmario-junior@hotmail.com>
2026-01-06 00:57:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        print("POST RECEBIDO")
-
-        if "file" not in request.files:
-            print("ERRO: request.files vazio")
-            return render_template("index.html")
-
-        file = request.files["file"]
-
-        if file.filename == "":
-            print("ERRO: filename vazio")
-            return render_template("index.html")
-
-        print("Arquivo recebido:", file.filename)
-
-        df = pd.read_excel(file)
-        print("Excel lido com sucesso")
-
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
